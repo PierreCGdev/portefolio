b5fffd61fbe55446fda68ee816149a9563769c02
pierreC <pierrecgdev@gmail.com>
2025-07-28 12:15:24 +0200

    retour masque 3d

--- components/CubeCanvas.jsx
+++ components/CubeCanvas.jsx
@@ -2,8 +2,7 @@ import { Canvas } from "@react-three/fiber";
 export default function CubeCanvas({ children }) {
   return (
     <div className="w-full h-full flex items-center justify-center relative">
-      {/* affiché sur desktop */}
-      <div className="hidden md:block w-full h-full">
+      <div className=" w-full h-full">
         <Canvas shadows camera={{ position: [0, 0, 29], fov: 22 }} dpr={1}>
           <ambientLight intensity={4} color={"#ffffff"} />
           <directionalLight
@@ -21,7 +20,7 @@ export default function CubeCanvas({ children }) {
       </div>
 
       {/* Affiché sur mobile */}
-      <div className="block md:hidden w-4/5 h-4/5 bg-zinc-900"></div>
+      {/* <div className="block md:hidden w-4/5 h-4/5 bg-zinc-900"></div> */}
     </div>
   );
 }
